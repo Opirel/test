a544b738808c1f66842c61a882e24c1e6a58dd19
Opirel <ofir12290@gmail.com>
2024-03-19 12:15:36 +0200

    update

--- base/views.py
+++ base/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 # Create your views here.
+
+ar=[{'name': "waga"}, {'name':'baga'}, {'name':'opir'}]
+
+
+@api_view(['GET'])
+def index(req):
+    return Response({'msg':'hello'})
+
+
+@api_view(['GET'])
+def get_data(req):
+
+    return Response(ar)
